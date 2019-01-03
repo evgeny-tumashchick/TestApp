863c154fa27ecdbd41453b20498b247204a6443a
evgeny.tumashchick <evgeny.tumashchick@moneyman.ru>
2019-01-03 18:35:28 +0300

    Add counter for count button on click action

--- app/src/main/java/com/tum/testapp/MainActivity.kt
+++ app/src/main/java/com/tum/testapp/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,5 +16,12 @@ class MainActivity : AppCompatActivity() {
   fun toastMe(view: View) {
     val myToast = Toast.makeText(this, "Hello Toast", Toast.LENGTH_SHORT)
     myToast.show()
+  }
+
+  fun countToastClick(view: View){
+    val countString = counterText.text.toString()
+    var count: Int = countString.toInt()
+    count++
+    counterText.text= count.toString()
   }
 }
